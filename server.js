409ccd338751c1a27d1023dabd57d8e49ed13a28
Marcelo <hmdelnegro@gmail.com>
2022-05-12 14:30:01 -0300

    fix: fixes

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const router = express.Router();
 const port = process.env.PORT || 3000;
 const defaultExpression = `
 This is a test template
-[if condition='1=1']Hello world![/if]
-and it was already processed if you can see "Hello world!"
+[if condition='1=1']Worked numbers[else]Worked numbers better![/if]
+[if condition='"abc"="abc"']Worked letters![else]Worked letters better![/if]
+[if condition='true=false']Worked booleans![else]Worked booleans better![/if]
+and its closing
 `;
 const defaultGrammar = fs.readFileSync('./grammar.txt', 'utf-8');
 const thlParser = peg.generate(defaultGrammar.toString());
